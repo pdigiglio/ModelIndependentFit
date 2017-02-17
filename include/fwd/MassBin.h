59dc755bb45eb426566b7236c15ae8cfbb59ba9e
pdigiglio <p.digiglio91@gmail.com>
2017-02-17 13:33:31 +0100

    [MassBin] add forward header

---
+++ include/fwd/MassBin.h
@@ -0,0 +1,17 @@
+/**
+ *
+ *    @file  MassBin.h
+ *   @brief  
+ *
+ *    @date  02/16/17
+ *  @author  Paolo Di Giglio (github.com/pdigiglio),
+ *           <p.digiglio91@gmail.com>
+ *
+ */
+
+#ifndef  FWD_MASS_BIN_H
+#define  FWD_MASS_BIN_H
+
+class MassBin;
+
+#endif
